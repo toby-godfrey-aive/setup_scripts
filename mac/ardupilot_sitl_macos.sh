8ecb04c5f930e70152ad3f190ab3e85b170f3808
Toby Godfrey <toby.g@aivesystems.com>
2025-08-26 14:16:38 +0100

    Update ardupilot_sitl_macos.sh

--- mac/ardupilot_sitl_macos.sh
+++ mac/ardupilot_sitl_macos.sh
@@ -405,6 +405,35 @@ setup_build_environment() {
     fi
 }
 
+alias_with_venv() {
+    local venv_path="$HOME/ardupilot_venv"
+    local alias_name="sim_vehicle"
+    local command_to_alias="source $venv_path/bin/activate && sim_vehicle.py"
+    local shell_rc_file=""
+
+    # Determine the shell RC file
+    if [[ "$SHELL" == *"zsh"* ]]; then
+        shell_rc_file="$HOME/.zshrc"
+    elif [[ "$SHELL" == *"bash"* ]]; then
+        shell_rc_file="$HOME/.bash_profile"
+    else
+        warn "Unsupported shell: $SHELL. Adding alias to ~/.profile instead."
+        shell_rc_file="$HOME/.profile"
+    fi
+
+    # Check if the alias already exists in the RC file
+    if grep -q "alias $alias_name=" "$shell_rc_file"; then
+        info "Alias '$alias_name' already exists in $shell_rc_file. Skipping..."
+    else
+        # Add the alias to the RC file
+        echo "alias $alias_name=\"$command_to_alias\"" >> "$shell_rc_file"
+        success "Added alias '$alias_name' to $shell_rc_file."
+    fi
+
+    # Inform the user to reload the shell
+    info "Please reload your shell with 'source $shell_rc_file' or restart your terminal."
+}
+
 
 setup_path_environment() {
     info "Setting up PATH environment..."
@@ -540,6 +569,7 @@ main() {
     clone_ardupilot
     run_prereqs_script
     setup_build_environment
+    alias_with_venv
     setup_path_environment
     handle_mojave_specifics
     test_installation
